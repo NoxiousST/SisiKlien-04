2ed0fe8dbe68d5073a0000cb8fa965e40f83e486
NoxiousST <danielkristian268@gmail.com>
2024-04-21 17:37:34 +0700

    Initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" style={{marginBottom: 20}} alt="logo"/>
+                <code style={{marginBottom: 20}}>Tugas 04</code>
+                <code>NIM: A11.2021.13429</code>
+                <code>Nama: Daniel Kristian Jatmiko</code>
+            </header>
+        </div>
+    );
+}
+
+export default App;
